321c810065fdce7299a3ac3a21a21f24c01f1c40
Om4rit0 <om4rit0.edu@gmail.com>
2022-11-02 23:17:52 +0200

    Reordered Ch3.1: Programming Exerices 4

--- CH3/exercices/ch3_exer_31_4.c
+++ CH3/exercices/ch3_exer_31_4.c
@@ -3,46 +3,71 @@
 */
 #include <stdio.h> /* printf, scanf definitions */
 #define PI 3.14159
+
+/* Global Variables */
+double hole_diameter; // input - diameter of hole
+double edge_diameter; // input - diameter of outer edge
+double thickness;    // input - thickness of washer
+double density;    // input - density of material used
+double quantity;    // input - number of washers made
+double weight;    // output - weight of washer batch
+double hole_radius;    // radius of hole
+double edge_radius;    // radius of outer edge
+double rim_area;    // area of rim
+double unit_weight;    // weight of 1 washer
+
+/* Functions Prototypes */
+void get_washer();
+void get_material();
+void set_rim();
+void total_weight();
+
 int main(void)
 {
-    double hole_diameter; // input - diameter of hole
-    double edge_diameter; // input - diameter of outer edge
-    double thickness;    // input - thickness of washer
-    double density;    // input - density of material used
-    double quantity;    // input - number of washers made
-    double weight;    // output - weight of washer batch
-    double hole_radius;    // radius of hole
-    double edge_radius;    // radius of outer edge
-    double rim_area;    // area of rim
-    double unit_weight;    // weight of 1 washer
+    get_washer();
+    get_material();
+    set_rim();
+    total_weight();
+    
+    /* Display the weight of the batch of washers */
+    printf("\nThe expected weight of the batch is %.2f", weight);
+    printf(" grams.\n");
+}
 
-    /* Get the inner diameter, outer diameter, and thickness.*/
+void get_washer()
+{
+    /* Get the inner diameter, outer diameter, and thickness*/
     printf("Inner diameter in centimeters> ");
     scanf("%lf", &hole_diameter);
     printf("Outer diameter in centimeters> ");
     scanf("%lf", &edge_diameter);
     printf("Thickness in centimeters> ");
     scanf("%lf", &thickness);
+}
 
-    /* Get the material density and quantity manufactured. */
+void get_material()
+{
+    /* Get the material density and quantity manufactured */
     printf("Material density in grams per cubic centimeter> ");
     scanf("%lf", &density);
     printf("Quantity in batch> ");
     scanf("%lf", &quantity);
+}
 
-    /* Compute the rim area. */
+void set_rim()
+{
+    /* Compute the rim area */
     hole_radius = hole_diameter / 2.0;
     edge_radius = edge_diameter / 2.0;
     rim_area = PI * edge_radius * edge_radius -
     PI * hole_radius * hole_radius;
+}
 
-    /* Compute the weight of a flat washer. */
+void total_weight()
+{
+   /* Compute the weight of a flat washer */
     unit_weight = rim_area * thickness * density;
 
-    /* Compute the weight of the batch of washers. */
+    /* Compute the weight of the batch of washers */
     weight = unit_weight * quantity;
-
-    /* Display the weight of the batch of washers. */
-    printf("\nThe expected weight of the batch is %.2f", weight);
-    printf(" grams.\n");
 }
