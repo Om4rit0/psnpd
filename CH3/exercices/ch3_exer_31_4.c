1948a8b7161ce86aa8d5dbb1e6b45c1d36cb1611
Om4rit0 <om4rit0.edu@gmail.com>
2022-11-03 16:09:07 +0200

    CH3.1: Programming 4: Implented Extension Function

--- CH3/exercices/ch3_exer_31_4.c
+++ CH3/exercices/ch3_exer_31_4.c
@@ -1,26 +1,53 @@
 /*
 * Computes the weight of a batch of flat washers.
+* 
+* Formulas:
+*   Edge Area = PI * (outer_radius)^2
+*   Inner Area = PI * (inner_radius)^2
+*   Rim Area = Edge Area - Inner Area
+*   
+*   Washer Volume = Rim Area * Thickness
+*   Material Area = Length * Width
+*   Material Volume = Material Area * Thickness
+*   Leftover Volume = Material Volume * Washer Volume
+* 
+*   Leftover Weight = Leftover Volume * Density 
 */
-#include <stdio.h> /* printf, scanf definitions */
+#include <stdio.h> // printf, scanf definitions
+#include <math.h>  // pow definition
+
 #define PI 3.14159
 
 /* Global Variables */
-double hole_diameter; // input - diameter of hole
-double edge_diameter; // input - diameter of outer edge
-double thickness;    // input - thickness of washer
-double density;    // input - density of material used
-double quantity;    // input - number of washers made
-double weight;    // output - weight of washer batch
-double hole_radius;    // radius of hole
-double edge_radius;    // radius of outer edge
-double rim_area;    // area of rim
-double unit_weight;    // weight of 1 washer
+    double hole_diameter; // input - diameter of hole
+    double edge_diameter; // input - diameter of outer edge
+    double thickness;     // input - thickness of washer
+    double density;       // input - density of material used
+    double quantity;      // input - number of washers made
+    double weight;        // output - weight of washer batch
+    double hole_radius;   // radius of hole
+    double edge_radius;   // radius of outer edge
+    double rim_area;      // area of rim
+    double unit_weight;   // weight of 1 washer
+
+/* Extension Variables */
+    double washer_unit_volume;   // volume of a single washer
+    double material_length;      // length of material's cuboid
+    double material_width;       // width of material's cuboid
+    double material_unit_area;   // area of a single cuboid of material
+    double material_unit_volume; // volume of a single cuboid of material
+    double leftover_volume;      // volume of material leftover
+    double leftover_weight;      // weight of material leftover
 
 /* Functions Prototypes */
-void get_washer();
-void get_material();
-void set_rim();
-void total_weight();
+    void get_washer();
+    void get_material();
+    void set_rim();
+    void total_weight();
+
+/* Extension Functions Prototypes */
+    void set_volumes();
+    void set_weights();
 
 int main(void)
 {
@@ -47,6 +74,15 @@ void get_washer()
 
 void get_material()
 {
+    /* Get the material cuboid's length and width */
+    printf("Material Length> ");
+    scanf("%lf", &material_length);
+    
+    printf("Material Width");
+    scanf("%lf", &material_width);
+    
+    
+    
     /* Get the material density and quantity manufactured */
     printf("Material density in grams per cubic centimeter> ");
     scanf("%lf", &density);
@@ -59,8 +95,8 @@ void set_rim()
     /* Compute the rim area */
     hole_radius = hole_diameter / 2.0;
     edge_radius = edge_diameter / 2.0;
-    rim_area = PI * edge_radius * edge_radius -
-    PI * hole_radius * hole_radius;
+    rim_area = PI * pow(edge_diameter, 2) -
+    PI * pow(hole_radius, 2);
 }
 
 void total_weight()
@@ -70,4 +106,18 @@ void total_weight()
 
     /* Compute the weight of the batch of washers */
     weight = unit_weight * quantity;
+}
+
+void set_volumes()
+{
+    washer_unit_volume = rim_area * thickness;
+    material_unit_area = material_length * material_width;
+    
+    material_unit_volume = material_unit_area * thickness;
+    
+}
+void set_weights()
+{
+    leftover_volume = material_unit_volume - unit_weight;
+    leftover_weight = leftover_volume * density;
 }
