8f319574a1dfa476c01cb93c29fe948d78e65a2f
Om4rit0 <om4rit0.edu@gmail.com>
2022-11-30 15:33:05 +0200

    Chapter 4: Programming Exercices 1: Incomplete Base

---
+++ CH4/exercices/ch4_exer_4.3_1.c
@@ -0,0 +1,60 @@
+#incldue <stdio.h>
+
+int main(void)
+{
+
+  /* Declare Variables*/
+  double age,
+         water,
+         year,
+         speed,
+         x, y, z,
+         w;
+
+  int condition_test_age,
+      condition_test_water,
+      condition_test_year,
+      condition_test_speed,
+      condition_test_variables,
+      condition_test_w,
+
+
+  /* Get Inputs */
+  printf("> ");
+  scanf(" %", &);
+
+  printf("> ");
+  scanf(" %", &);
+ 
+  printf("> ");
+  scanf(" %", &);
+ 
+  printf("> ");
+  scanf(" %", &);
+  
+  printf("> ");
+  scanf(" %", &);
+
+  printf("> ");
+  scanf(" %", &);
+
+  printf("> ");
+  scanf(" %", &);
+
+  /* Evaluate Conditions */
+  condition_test_age = ((18 <= age) && (age <= 21));
+  condition_test_water = ;
+  condition_test_year = ;
+  condition_test_speed = ;
+  condition_test_variables = ;
+  condition_test_w = ;
+
+
+  /* Print Formated Outputs */
+  printf("", condition_test_);
+  printf("", condition_test_);
+  printf("", condition_test_);
+  printf("", condition_test_);
+  printf("", condition_test_);
+  printf("", condition_test_);
+}
