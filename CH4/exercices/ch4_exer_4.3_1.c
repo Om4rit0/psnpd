08506a663cfdf52d2ee42c2efaf25459f9f21093
Om4rit0 <om4rit0.edu@gmail.com>
2022-12-01 15:01:40 +0200

    Chapter 4: Programming Exercices 1: Fixed Error (semicolon instead of comma)

--- CH4/exercices/ch4_exer_4.3_1.c
+++ CH4/exercices/ch4_exer_4.3_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
 
   /* Declare Variables*/
@@ -16,46 +16,47 @@ int main()
       condition_test_year,
       condition_test_speed,
       condition_test_variables,
-      condition_test_w,
+      condition_test_w;
 
 
   /* Get Inputs */
-  printf("Enter Age> ");
-  scanf(" %lf", &age);
+    printf("Enter Age> ");
+    scanf("%lf", &age);
 
-  printf("Enter water> ");
-  scanf(" %lf", &water);
- 
-  printf("year> ");
-  scanf(" %lf", &year);
- 
-  printf("speed> ");
-  scanf(" %lf", &speed);
-  
-  printf("variable X> ");
-  scanf(" %lf", &x);
-  printf("variable Y> ");
-  scanf(" %lf", &y);
-  printf("variable Z> ");
-  scanf(" %lf", &z);
+    printf("Enter water> ");
+    scanf("%lf", &water);
 
-  printf("Enter W> ");
-  scanf(" %lf", &w);
+    printf("year> ");
+    scanf("%lf", &year);
+ 
+    printf("speed> ");
+    scanf("%lf", &speed);
+
+    printf("variable X> ");
+    scanf("%lf", &x);
+    printf("variable Y> ");
+    scanf("%lf", &y);
+    printf("variable Z> ");
+    scanf("%lf", &z);
+
+    printf("Enter W> ");
+    scanf("%lf", &w);
+    
 
   /* Evaluate Conditions */
-  condition_test_age = ((18 <= age) && (age <= 21));       // Evaluating Age Condition
-  condition_test_water = ((water < 1.5) && (0.1 < water)); // Evaluating Water Condition
-  condition_test_year = (n % 4 == 0);                      // Evaluating Year Condition
-  condition_test_speed = (!(speed > 55));                  // Evaluating Speed Condition
-  condition_test_variables = ((y < z) && (x < y));         // Evaluating Variables Condition
-  condition_test_w = ((w == 6) || !(w > 3));               // Evaluating W Condition
+    condition_test_age = ((18 <= age) && (age <= 21));       // Evaluating Age Condition
+    condition_test_water = ((water < 1.5) && (0.1 < water)); // Evaluating Water Condition
+    condition_test_year = (n % 4 == 0);                      // Evaluating Year Condition
+    condition_test_speed = (!(speed > 55));                  // Evaluating Speed Condition
+    condition_test_variables = ((y < z) && (x < y));         // Evaluating Variables Condition
+    condition_test_w = ((w == 6) || !(w > 3));               // Evaluating W Condition
 
 
   /* Print Formated Outputs */
-  printf("Age: %i", condition_test_age);
-  printf("Water: %i", condition_test_water);
-  printf("Year: %i", condition_test_year);
-  printf("Speed: %i", condition_test_speed);
-  printf("Variables: %i", condition_test_variables);
-  printf("W: %i", condition_test_w);
+    printf("Age: %i\n", condition_test_age);
+    printf("Water: %i\n", condition_test_water);
+    printf("Year: %i\n", condition_test_year);
+    printf("Speed: %i\n", condition_test_speed);
+    printf("Variables: %i\n", condition_test_variables);
+    printf("W: %i\n", condition_test_w);
 }
